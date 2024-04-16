fbacc963456e3c3ae36fe327198dfe26d8f578dc
asionsolver <asionsolver274@gmail.com>
2024-04-17 01:45:38 +0600

    add new file name like stringAt.js

---
+++ stringAt.js
@@ -0,0 +1,32 @@
+let text = "W3Schools";
+let character1 = text.at(-1);
+let character2 = text.at();
+let character3 = text.at(3);
+console.log(character1); // Output: s
+console.log(character2); // Output: W
+console.log(character3); // Output: c
+
+// let text = "W3Schools";
+// let character = text[0];
+// console.log(character); // Output: W
+
+
+/**
+@Javascript At method
+
+The at() method returns the character at a specified index (position) in a string.
+The at() method returns the same as [].
+
+Syntax
+string.at(index)
+
+Parameters
+1. index(Optional).
+2. The index (position) of the character to be returned.
+3. Default is 0.
+4. -1 returns the last character.
+
+Return Value
+1. A String ----> A single character.
+2. The character in the given position (index) in the string.
+*/
